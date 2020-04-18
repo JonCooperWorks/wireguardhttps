2aa6d4165f90b860b33604ba9a43d15a91cfc692
Jonathan Cooper <jonathan@targetpractice.network>
2020-04-18 15:30:17 +0000

    Added PublicKey to Device model

--- models.go
+++ models.go
@@ -4,25 +4,36 @@ import (
 	"net"
 
 	"github.com/jinzhu/gorm"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
+// IPAddress is a single IP address.
+// IPAddresses are meant to be allocated at program initialization and all stored in the database.
+// For example, if wireguardhttps is initialized with the subnet 10.0.0.0/24, an entry will be created in this table for every IP address between 10.0.0.0 and 10.0.0.255.
 type IPAddress struct {
 	gorm.Model
 	Address net.IP `gorm:"PRIMARY_KEY"`
 }
 
+// Device is a connected Wireguard peer.
+// Devices must be assigned an unassigned IP address from the `IPAddress` table
+// Each device must have a unique IP address and public key, and we use the UNIQUE SQL constraint to enforce this.
 type Device struct {
 	gorm.Model
 	IP        IPAddress `gorm:"foreignkey:IPAddress"`
 	IPAddress net.IP    `gorm:"UNIQUE"`
 	Name      string
 	OS        string
-	User      User
+	User      UserProfile
+	PublicKey wgtypes.Key `gorm:"UNIQUE"`
 }
 
-type User struct {
+// UserProfile represents a user who authenticated using an OpenID integration.
+// We maintain as little information as possible about users to make this application a less attractive target to hackers.
+type UserProfile struct {
 	gorm.Model
-	Username          string
-	Name              string
-	UserPrincipalName string
+	Name               string
+	Email              string
+	AuthPlatformUserID string
+	AuthPlatform       string
 }
