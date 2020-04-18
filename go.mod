0f6941fa238d89f7c7f3e6a3f5847065a029b40b
Jonathan Cooper <jonathan@targetpractice.network>
2020-04-18 01:56:28 +0000

    Added database models

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/joncooperworks/wireguardhttps
 
 go 1.14
+
+require github.com/jinzhu/gorm v1.9.12
--- models.go
+++ models.go
@@ -1,1 +1,27 @@
 package wireguardhttps
+
+import (
+	"net"
+
+	"github.com/jinzhu/gorm"
+)
+
+type IPAddress struct {
+	gorm.Model
+	Address net.IP `gorm:"PRIMARY_KEY"`
+}
+
+type Device struct {
+	gorm.Model
+	IPAddress IPAddress `gorm:"UNIQUE"`
+	Name      string
+	OS        string
+	User      User
+}
+
+type User struct {
+	gorm.Model
+	Username          string
+	Name              string
+	UserPrincipalName string
+}
